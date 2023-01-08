27edf002abf35c5e01dc9e4d9f0c09ec285a93a4
mertparlak-zima <mertparlak123@hotmail.com>
2023-01-08 17:15:49 +0300

    First build app.

---
+++ app/src/main/java/app/zimablue/besocial/MainActivity.kt
@@ -0,0 +1,22 @@
+package app.zimablue.besocial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import app.zimablue.besocial.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+
+
+    }
+}
